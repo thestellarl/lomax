f356f198c173bbd545a732d91f15637bfa55e101
Lucas <stellal@oregonstate.edu>
2020-03-08 15:59:54 -0700

    server update

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const express = require('express');
 var exphbs  = require('express-handlebars');
+var session = require('express-session')
 
 const app = express();
-const port = 3000;
+var port = process.env.PORT || 3000;
+
+var userData = require('./userData.json');
+
+const userMap = {};
+userData.forEach(user => {
+    userMap[user.uuid] = user;
+});
+
+const refreshUserMap = () => {
+    userMap = {};
+    var user = fs.readFileSync(require.resolve('./userData.json'));
+    userData.forEach(user => {
+        userMap[user.uuid] = user;
+    });
+};
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
- 
+app.use(express.static('public'))
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+  }))
+
 app.get('/', function (req, res) {
     res.render('home');
 });
