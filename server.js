9c95a91d5335a6da6c3f2a19122a5ee0a6057d61
Lucas <stellal@oregonstate.edu>
2020-03-07 16:39:37 -0800

    studio page initial work

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const port = 3000;
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
- 
+app.use(express.static('public'))
+
 app.get('/', function (req, res) {
     res.render('home');
 });
